a501643f371d82e5abfc90ff92b7e9b44664cde2
SunPark <yangheat@gmail.com>
2024-10-21 12:52:52 +0900

    feat: install react-router-dom
    로그인, 회원가입, Todo 라우팅

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './pages/login/index.tsx'
+import Signup from './pages/signup/index.tsx'
+import Todo from './pages/todos/index.tsx'
+
+const router = createBrowserRouter([{
+    path: '/',
+    element: <Login />
+  }, {
+    path: '/login',
+    element: <Login />
+  }, {
+    path: '/signup',
+    element: <Signup />
+  }, {
+    path: '/todos',
+    element: <Todo />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
