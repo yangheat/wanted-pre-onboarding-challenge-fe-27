5e8c08f627e607e67a634b582bc8af05f494fc27
SunPark <yangheat@gmail.com>
2024-10-22 12:32:52 +0900

    fix: change the todos path to todo

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './pages/login/index.tsx'
 import Signup from './pages/signup/index.tsx'
-import Todo from './pages/todos/index.tsx'
+import Todo from './pages/todo/index.tsx'
 
 const router = createBrowserRouter([{
-    path: '/',
-    element: <Login />
-  }, {
-    path: '/login',
-    element: <Login />
-  }, {
-    path: '/signup',
-    element: <Signup />
-  }, {
-    path: '/todos',
-    element: <Todo />
-  }
+  path: '/',
+  element: <Login />
+}, {
+  path: '/login',
+  element: <Login />
+}, {
+  path: '/signup',
+  element: <Signup />
+}, {
+  path: '/todo',
+  element: <Todo />
+}
 ])
 
 createRoot(document.getElementById('root')!).render(
--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -1,3 +1,3 @@
 export default function Todo () {
-    return 'todos page'
+    return 'todo page'
 }
